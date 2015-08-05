b67b3529c04db9d068344ede0c75adb57ff75136
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-05 09:23:33 -0700

    Add LoginService Controller and Twitter Service Controller.

---
+++ sead40_hw1/LoginService.swift
@@ -0,0 +1,18 @@
+//
+//  LoginService.swift
+//  sead40_hw1
+//
+//  Created by Joao Paulo Galvao Alves on 8/5/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import Foundation
+import Accounts
+
+class LoginService {
+  class func loginForTwitter(completionHandler: (String, ACAccount) -> Void) {
+  
+  }
+}
+
+
