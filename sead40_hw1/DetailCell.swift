eb3c615bfaaeb727c4faf68b3dfa2d131022a798
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-06 11:51:51 -0700

    Add DetailCell and its outlets.

---
+++ sead40_hw1/DetailCell.swift
@@ -0,0 +1,22 @@
+//
+//  DetailCell.swift
+//  sead40_hw1
+//
+//  Created by Joao Paulo Galvao Alves on 8/6/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import UIKit
+
+class DetailCell: UITableViewCell {
+
+  
+  
+  @IBOutlet weak var retweetedByLabel: UILabel!
+  @IBOutlet weak var userLabel: UILabel!
+  
+  @IBOutlet weak var userAddress: UILabel!
+  
+  @IBOutlet weak var tweetTextLabel: UILabel!
+  
+}
