fc698e919bdeb27fc9c2c672ce5282fb4af1f8c6
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-05 20:30:21 -0700

    Add .Ds_Store, TweetCell and changes to the cell on storyboard.

---
+++ sead40_hw1/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  sead40_hw1
+//
+//  Created by Joao Paulo Galvao Alves on 8/5/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
