03cdda3c608248460624f4aeb1dcceba4166fae5
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-06 08:38:30 -0700

    Add detail viewController on storyboard.

---
+++ sead40_hw1/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  sead40_hw1
+//
+//  Created by Joao Paulo Galvao Alves on 8/6/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import Foundation
